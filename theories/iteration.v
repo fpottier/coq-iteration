9c1e017f335b520c817173419e18a0b471e4acda
François Pottier <francois.pottier@inria.fr>
2024-03-07 15:06:47 +0100

    Some Coq work in iteration.v.
    Needs to be cleaned up and split into multiple files.

---
+++ theories/iteration.v
@@ -0,0 +1,381 @@
+Require Import Utf8.
+Require Import Coq.Program.Equality.
+Require Import List. Import ListNotations.
+Require Import stdpp.orders.
+Set Primitive Projections.
+
+(* -------------------------------------------------------------------------- *)
+
+(* TODO split this part into listSnoc.v *)
+
+(* The [snoc] function on lists. *)
+
+(* TODO need infix notation for snoc *)
+
+Lemma cons_is_app {A} x (xs : list A) :
+  x :: xs = [x] ++ xs.
+Proof.
+  reflexivity.
+Qed.
+
+Definition snoc {A} (xs : list A) (x : A) :=
+  xs ++ [x].
+
+Lemma rev_cons {A} (xs : list A) (x : A) :
+  rev (x :: xs) = snoc (rev xs) x.
+Proof.
+  reflexivity.
+Qed.
+
+Lemma rev_snoc {A} (xs : list A) (x : A) :
+  rev (snoc xs x) = x :: rev xs.
+Proof.
+  unfold snoc. rewrite rev_app_distr. reflexivity.
+Qed.
+
+Lemma app_cons_eq_snoc_app {A} (xs ys : list A) y :
+  xs ++ y :: ys  = snoc xs y ++ ys.
+Proof.
+  unfold snoc. rewrite <- app_assoc. reflexivity.
+Qed.
+
+Lemma qwd {A} (xs : list A) x y :
+  x :: snoc xs y = snoc (x :: xs) y.
+Proof.
+  unfold snoc. rewrite (cons_is_app x xs), <- app_assoc. reflexivity.
+Qed.
+
+(* -------------------------------------------------------------------------- *)
+
+(* TODO split this part into listPrefix.v *)
+
+(* The [prefix] relation on lists. *)
+
+Inductive prefix {A} : list A → list A → Prop :=
+| PrefixZero:
+    ∀ xs,
+    prefix xs xs
+| PrefixSucc:
+    ∀ xs x zs,
+    prefix (snoc xs x) zs →
+    prefix xs zs.
+
+Global Hint Constructors prefix : prefix.
+
+Module PrefixNotations.
+  Infix "⊆" := prefix (at level 70, no associativity).
+End PrefixNotations.
+Import PrefixNotations.
+
+Lemma prefix_transitive {A} (xs ys : list A) :
+  xs ⊆ ys →
+  ∀ zs,
+  ys ⊆ zs →
+  xs ⊆ zs.
+Proof.
+  induction 1; intros; eauto with prefix.
+Qed.
+
+Lemma prefix_snoc {A} (xs : list A) x :
+  xs ⊆ snoc xs x.
+Proof.
+  eauto with prefix.
+Qed.
+
+Lemma prefix_app {A} :
+  ∀ ys xs : list A,
+  xs ⊆ xs ++ ys.
+Proof.
+  induction ys; intros;
+  rewrite ?app_nil_r, ?app_cons_eq_snoc_app;
+  eauto with prefix.
+Qed.
+
+Local Hint Resolve prefix_app : prefix.
+
+(* -------------------------------------------------------------------------- *)
+
+(* TODO split this part *)
+
+(* The reflexive-transitive closure of a labeled transition relation. *)
+
+Inductive rtcl {S A} (R : S → A → S → Prop) : S → list A → S → Prop :=
+| RtclNil:
+    ∀ s,
+    rtcl R s [] s
+| RtclCons:
+    ∀ s1 s2 s3 x xs,
+    R s1 x s2 →
+    rtcl R s2 xs s3 →
+    rtcl R s1 (x :: xs) s3
+.
+
+Global Hint Constructors rtcl : rtcl.
+
+Lemma rtcl_snoc {S A} (R : S → A → S → Prop) :
+  ∀ s1 s2 s3 x xs,
+  rtcl R s1 xs s2 →
+  R s2 x s3 →
+  rtcl R s1 (snoc xs x) s3.
+Proof.
+  induction 1; unfold snoc; simpl; eauto with rtcl.
+Qed.
+
+Global Hint Resolve rtcl_snoc : rtcl.
+
+Inductive rtcl' {S A} (R : S → A → S → Prop) : S → list A → S → Prop :=
+| Rtcl'Nil:
+    ∀ s,
+    rtcl' R s [] s
+| Rtcl'Cons:
+    ∀ s1 s2 s3 x xs,
+    rtcl' R s1 xs s2 →
+    R s2 x s3 →
+    rtcl' R s1 (snoc xs x) s3
+.
+
+Global Hint Constructors rtcl' : rtcl'.
+
+Lemma rtcl'_cons {S A} (R : S → A → S → Prop) :
+  ∀ s2 xs s3,
+  rtcl' R s2 xs s3 →
+  ∀ s1 x,
+  R s1 x s2 →
+  rtcl' R s1 (x :: xs) s3.
+Proof.
+  induction 1; simpl; intros.
+  { change [x] with (snoc [] x). eauto with rtcl'. }
+  { rewrite qwd. eauto with rtcl'. }
+Qed.
+
+Global Hint Resolve rtcl'_cons : rtcl'.
+
+Lemma rtcl_rtcl' {S A} (R : S → A → S → Prop) :
+  ∀ s xs s',
+  rtcl R s xs s' →
+  rtcl' R s xs s'.
+Proof.
+  induction 1; simpl; eauto with rtcl'.
+Qed.
+
+(* -------------------------------------------------------------------------- *)
+
+(* TODO unused; throw away? *)
+
+Definition mirror {S A} (R : S → A → S → Prop) : S → A → S → Prop :=
+  λ s a s', R s' a s.
+
+Lemma mirror_mirror {S A} (R : S → A → S → Prop) :
+  mirror (mirror R) = R.
+Proof.
+  reflexivity.
+Qed.
+
+Lemma rtcl_mirror {S A} (R : S → A → S → Prop) :
+  ∀ s xs s',
+  rtcl R s xs s' →
+  rtcl (mirror R) s' (rev xs) s.
+Proof.
+  induction 1; rewrite ?rev_cons; eauto with rtcl.
+Qed.
+
+(* -------------------------------------------------------------------------- *)
+
+(* A definition of iteration spaces in terms of two predicates
+   [permitted] and [complete], following Pereira and Filliâtre. *)
+
+(* TODO comment, clean up, etc. *)
+
+Record space A := {
+  permitted : list A → Prop;
+  complete  : list A → Prop;
+  permitted_empty : permitted [];
+  permitted_closed : ∀ xs zs, permitted zs → xs ⊆ zs → permitted xs;
+  complete_permitted : ∀ xs, complete xs → permitted xs;
+}.
+
+Local Obligation Tactic :=
+  (simpl; eauto).
+
+Arguments permitted {A} _.
+Arguments complete  {A} _.
+
+Definition subspace {A} (σ1 σ2 : space A) :=
+  (∀ xs, permitted σ1 xs → permitted σ2 xs) ∧
+  (∀ xs, complete σ1 xs → complete σ2 xs).
+
+Local Infix "⊑" := subspace (at level 70, no associativity).
+
+Global Instance subspace_reflexive : ∀ A, Reflexive (@subspace A).
+Proof. econstructor; eauto. Qed.
+
+Global Instance subspace_transitive : ∀ A, Transitive (@subspace A).
+Proof. unfold subspace. econstructor; intuition eauto. Qed.
+
+Definition eqspace {A} (σ1 σ2 : space A) :=
+  σ1 ⊑ σ2 ∧ σ2 ⊑ σ1.
+
+Local Infix "≡" := eqspace (at level 70, no associativity).
+
+Program Definition top {A} : space A :=
+  {| permitted xs := True; complete xs := True |}.
+
+(* -------------------------------------------------------------------------- *)
+
+(* A definition of iteration spaces in terms of automata. *)
+
+(* These are not necessarily finite-state automata. *)
+
+Record automaton A := {
+  state     : Type;
+  initial   : state;
+  step      : state → A → state → Prop;
+  final     : state → Prop;
+}.
+
+Arguments state   {A} _.
+Arguments initial {A} _.
+Arguments step    {A} _.
+Arguments final   {A} _.
+
+Definition steps {A} (α : automaton A) : state α → list A → state α → Prop :=
+  rtcl (step α).
+
+(* -------------------------------------------------------------------------- *)
+
+(* [s2a] converts an iteration space to an automaton. *)
+
+Definition s2a {A} (σ : space A) : automaton A :=
+  {|
+    state   := list A;
+    initial := [];
+    step xs x xs' := (snoc xs x = xs') ∧ permitted σ xs';
+    final   := complete σ
+  |}.
+
+(* -------------------------------------------------------------------------- *)
+
+(* [a2s] converts an automaton to an iteration space. *)
+
+Program Definition a2s {A} (α : automaton A) : space A :=
+  {|
+    permitted xs := ∃ s, steps α (initial α) xs s;
+    complete  xs := ∃ s, steps α (initial α) xs s ∧ final α s;
+  |}.
+Next Obligation.
+  unfold steps. eauto with rtcl.
+Qed.
+Next Obligation.
+Admitted.
+Next Obligation.
+  firstorder.
+Qed.
+
+(* -------------------------------------------------------------------------- *)
+
+(* TODO clean up *)
+
+Lemma foo {A} (σ : space A) :
+  let α := s2a σ in
+  ∀ xs s,
+  steps α (initial α) xs s →
+  s = xs.
+Proof.
+Admitted.
+
+Local Lemma edge_target_is_permitted {A} (σ : space A) xs x xs' :
+  step (s2a σ) xs x xs' →
+  permitted σ xs'.
+Proof.
+  unfold s2a. simpl. tauto.
+Qed.
+
+Local Lemma steps_self_append {A} (σ : space A) :
+  ∀ ys xs,
+  permitted σ (xs ++ ys) →
+  steps (s2a σ) xs ys (xs ++ ys).
+Proof.
+  induction ys as [| y ys ]; intros xs.
+  { intros _.
+    rewrite app_nil_r.
+    unfold s2a, steps, step.
+    eauto with rtcl. }
+  { rewrite app_cons_eq_snoc_app.
+    intros Hpermitted.
+    econstructor; [| eapply IHys; eauto ].
+    unfold s2a, step.
+    split; [ reflexivity |].
+    eapply permitted_closed; eauto with prefix. }
+Qed.
+
+Local Lemma steps_self {A} (σ : space A) :
+  ∀ xs,
+  permitted σ xs →
+  steps (s2a σ) [] xs xs.
+Proof.
+  eauto using steps_self_append.
+Qed.
+
+(* -------------------------------------------------------------------------- *)
+
+(* The lemma [roundtrip1] states that if an iteration space [σ] is
+   first converted to an automaton, and if this automaton is then
+   converted back to an iteration space, then the result is [σ]
+   again, up to equivalence [≡]. *)
+
+Lemma roundtrip1l {A} (σ : space A) :
+  a2s (s2a σ) ⊑ σ.
+Proof.
+  unfold a2s. econstructor; simpl; intros xs.
+
+  (* Inclusion of the [permitted] components. *)
+  (* Goal: if in the automaton, out of the initial state, there is
+     a path labeled [xs], then [xs] is permitted. *)
+  { intros (s & Hsteps).
+    assert (s = xs); [| subst s ].
+    { eapply foo. exact Hsteps. }
+    (* Either this path is empty, or it ends with an edge. *)
+    apply rtcl_rtcl' in Hsteps. dependent destruction Hsteps.
+    (* Case: the path is empty. *)
+    { eapply permitted_empty. }
+    (* Case: the path ends with an edge. *)
+    { clear Hsteps. eauto using edge_target_is_permitted. }
+  }
+
+  (* Inclusion of the [complete] components. *)
+  (* Goal: if in the automaton, out of the initial state, there is
+     a path labeled [xs] to a final state, then [xs] is complete. *)
+  { intros (s & Hsteps & ?).
+    assert (s = xs); [| subst s ].
+    { eapply foo. exact Hsteps. }
+    tauto. }
+Qed.
+
+Lemma roundtrip1r {A} (σ : space A) :
+  σ ⊑ a2s (s2a σ).
+Proof.
+  unfold a2s. econstructor; simpl; intros xs.
+
+  (* Inclusion of the [permitted] components. *)
+  (* Goal: if [xs] is permitted, then in the automaton, out of the
+     initial state, there is a path labeled [xs]. *)
+  { eauto using steps_self. }
+
+  (* Inclusion of the [complete] components. *)
+  (* Goal: if [xs] is complete then, in the automaton, out of the
+     initial state, there is a path labeled [xs] to a final state. *)
+  { eauto using steps_self, complete_permitted. }
+Qed.
+
+Lemma roundtrip1 {A} (σ : space A) :
+  a2s (s2a σ) ≡ σ.
+Proof.
+  split; eauto using roundtrip1l, roundtrip1r.
+Qed.
+
+(* -------------------------------------------------------------------------- *)
+
+Module SpaceNotations.
+  Infix "⊑" := subspace (at level 70, no associativity).
+  Infix "≡" := eqspace (at level 70, no associativity).
+End SpaceNotations.
